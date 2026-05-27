37715a7d94bd631a19bc7665db2aa9d44dd63b30
Ricksiker <ricardo.navarro3333@alumnos.udg.mx>
2026-05-27 10:06:42 -0600

    Actividad 4.3 - Curva de dificultad y zona de muerte dinamica

--- Assets/Scripts/ZonaMuerte.cs
+++ Assets/Scripts/ZonaMuerte.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ZonaMuerte : MonoBehaviour
 {
+    public GameObject efectoSplash;
+    public float delayReinicio = 1f;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            if (efectoSplash != null)
+                Instantiate(efectoSplash, other.transform.position, Quaternion.identity);
+
+            other.gameObject.SetActive(false);
+            StartCoroutine(ReiniciarEscena());
         }
+    }
+
+    IEnumerator ReiniciarEscena()
+    {
+        yield return new WaitForSeconds(delayReinicio);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
